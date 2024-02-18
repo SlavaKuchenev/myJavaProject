3a8b11f669a0aaf2810af44523e3f31541ab5e7a
SlavaKuchenev <102597605+SlavaKuchenev@users.noreply.github.com>
2024-02-18 10:37:46 +0300

    Update Lesson2.java

--- Lesson_2/src/main/java/School/lesson2/Lesson2.java
+++ Lesson_2/src/main/java/School/lesson2/Lesson2.java
@@ -8,15 +8,12 @@ public class Lesson2 {
         printColor();
         compareNumbersV1();
         compareNumbersV2();
-
-
-    }
+        }
+    
     public static void printThreeWords(){
-        System.out.println(" Orange ");
-        System.out.println(" Banana ");
-        System.out.println(" Apple ");
-
+        System.out.println("Orange/nBanana/nApple");
     }
+    
     public static void checkSumSignV1(){
         int a = 3;
         int b = 4;
@@ -27,12 +24,14 @@ public class Lesson2 {
             System.out.println("Сумма отрицательная");
         }
     }
+    
     public static void checkSumSignV2(){
         int a = 3;
         int b = 4;
         int sum = a + b;
         System.out.println(sum >= 0 ? "Сумма положительная" : "Сумма отрицательная");
     }
+    
     public static void printColor(){
         int value = 3;
         if (value < 0){
@@ -46,19 +45,21 @@ public class Lesson2 {
             }
         }
     }
+    
     public static void compareNumbersV1(){
         int a = 9999;
         int b = 4;
         if (a >= b){
             System.out.println("a >= b");
         } else {
-            System.out.println("a <= b");
+            System.out.println("a < b");
         }
     }
+    
     public static void compareNumbersV2(){
         int a = 3;
         int b = 4;
-        System.out.println(a >= b? " a >= b" : "a <= b");
+        System.out.println(a >= b? " a >= b" : "a < b");
     }
 
 }
